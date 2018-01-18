58594c4c2b53e44236309ef2138063e23687bddc
baileicheng <xbfighting@hotmail.com>
2018-01-18 13:58:47 +0800

    [mod] method override & new

---
+++ CSharpDailyLearning/ClassAndOO/NewAndOverride.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndOO
+{
+    public class A
+    {
+        public virtual string Print()
+        {
+            return "A";
+        }
+    }
+
+    public class B1 : A
+    {
+        public override string Print()
+        {
+            return "B1";
+        }
+    }
+
+    public class B2 : A
+    {
+        public new string Print()
+        {
+            return "B2";
+        }
+    }
+}
---
+++ CSharpDailyLearning/ClassAndOOTests/NewAndOverrideTests.cs
@@ -0,0 +1,68 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassAndOO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassAndOO.Tests
+{
+    [TestFixture]
+    public class NewAndOverrideTests
+    {
+
+        [Test]
+        public void Test_Override()
+        {
+            // given
+            string result = "B1";
+
+            // when
+            B1 b1 = new B1();
+
+            // then
+            Assert.AreSame(result, b1.Print());
+        }
+
+        [Test]
+        public void Test_New()
+        {
+            // given
+            string result = "B2";
+            
+            // when
+            B2 b2 = new B2();
+            
+            // then
+            Assert.AreSame(result, b2.Print());
+        }
+
+        [Test]
+        public void Test_Override_InitBase()
+        {
+            // given
+            string result = "B1";
+
+            // when
+            A ab1 = new B1();
+
+            // then
+            Assert.AreSame(result, ab1.Print());
+        }
+
+        [Test]
+        public void Test_New_InitBase()
+        {
+            // given
+            string result = "A";
+
+            // when
+            A ab2 = new B2();
+
+            // then
+            Assert.AreSame(result, ab2.Print());
+        }
+    }
+}

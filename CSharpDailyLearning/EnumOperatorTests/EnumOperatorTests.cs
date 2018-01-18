e054861b83fe9a6d60ee9c2c8dbf849eaee1a1cf
baileicheng <xbfighting@hotmail.com>
2018-01-18 11:24:34 +0800

    [mod] add NUnit unit test;

--- CSharpDailyLearning/EnumOperatorTests/EnumOperatorTests.cs
+++ CSharpDailyLearning/EnumOperatorTests/EnumOperatorTests.cs
@@ -1,38 +1,39 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EnumOperator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace EnumOperator.Tests
 {
-    [TestClass()]
+    [TestFixture]
     public class EnumOperatorTests
     {
-        [TestMethod()]
+        [Test]
         public void GetEnumDescriptionByInt_InEnum_Test()
         {
             var description = EnumOperator.GetEnumDescriptionByInt(1);
             Assert.IsTrue(description == PackageStoredTypeEnum.Normal.GetDescription());
         }
 
-        [TestMethod()]
+        [Test()]
         public void GetEnumDescriptionByInt_OutOfEnum_Test()
         {
             var description = EnumOperator.GetEnumDescriptionByInt(10);
             Assert.IsTrue(description == PackageStoredTypeEnum.UnKnown.GetDescription());
         }
 
-        [TestMethod()]
+        [Test()]
         public void GetEnumDescriptionByInt_OutOfEnumEx_Test()
         {
             var description = EnumOperator.GetEnumDescriptionByInt(-1);
             Assert.IsTrue(description == PackageStoredTypeEnum.UnKnown.GetDescription());
         }
 
-        [TestMethod()]
+        [Test()]
         public void GetEnumDescriptionByInt_Null_Test()
         {
             var description = EnumOperator.GetEnumDescriptionByInt(null);

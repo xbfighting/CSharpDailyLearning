2044a752a859c7195cf704a203a8b12a0f1f04c0
baileicheng <xbfighting@hotmail.com>
2018-03-12 17:53:00 +0800

    [mod delegate execise & unit test

--- CSharpDailyLearning/Delegate/DelegateActionFun.cs
+++ CSharpDailyLearning/Delegate/DelegateActionFun.cs
@@ -14,22 +14,9 @@ namespace Delegate
             return ruleFunc.Invoke(t1, t2);
         }
 
-        //Comparer<int> (int i1, int i2, )
-
         public static bool rule1(int t1, int t2)
         {
             return t1 - t2 > 0;
         }
-
-        public static void Main(string[] args)
-        {
-            var result = Compare(1, 2, rule1);
-            Console.WriteLine("Int compare result:{0}", result);
-        }
-    }
-
-    public class EventTest
-    {
-        
     }
 }
---
+++ CSharpDailyLearning/DelegateTests/DelegateActionFunTests.cs
@@ -0,0 +1,36 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Delegate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Assert = NUnit.Framework.Assert;
+
+namespace Delegate.Tests
+{
+    [TestFixture]
+    public class DelegateActionFunTests
+    {
+        [Test]
+        public void CompareTest()
+        {
+            // given 
+            int a = 1;
+            int b = 2;
+            Func<int, int, bool> ruleFunc = DelegateActionFun.rule1;
+            bool expected = false;
+
+            // when
+            var result = DelegateActionFun.Compare(a, b, ruleFunc);
+
+            // then 
+            Console.WriteLine("Int compare result:{0}", result);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

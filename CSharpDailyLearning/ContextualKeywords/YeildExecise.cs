2bff099b07f1400b578011efe044fc0f2f19ba3e
baileicheng <xbfighting@hotmail.com>
2018-03-12 17:37:26 +0800

    [mod] yeild

---
+++ CSharpDailyLearning/ContextualKeywords/YeildExecise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextualKeywords
+{
+    public class YeildExecise
+    {
+        /*
+           Yield has two great uses:
+           1、It helps to provide custom iteration without creating temp collections.
+           2、It helps to do stateful iteration.
+         */
+        public IEnumerable<int> Integers()
+        {
+            yield return 1;
+            yield return 12;
+            yield return 13;
+            yield return 14;
+            yield return 15;
+            yield return 16;
+            yield return 17;
+            yield return 188;
+        }
+
+        public IEnumerable<int> IntegerGenerator(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                yield return i;
+            }
+        }
+
+    }
+}
---
+++ CSharpDailyLearning/ContextualKeywordsTests/YeildExeciseTests.cs
@@ -0,0 +1,57 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContextualKeywords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ContextualKeywords.Tests
+{
+    [TestFixture]
+    public class YeildExeciseTests
+    {
+
+        [Test]
+        public void YeildTest01()
+        {
+            // given
+            foreach (var item in Integers())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private IEnumerable<int> Integers()
+        {
+            yield return 1;
+            yield return 12;
+            yield return 13;
+            yield return 14;
+            yield return 15;
+            yield return 16;
+            yield return 17;
+            yield return 188;
+        }
+
+
+        [Test]
+        public void YeildTest02()
+        {
+            // given
+            foreach (var item in IntegerGenerator(50*50))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private IEnumerable<int> IntegerGenerator(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}

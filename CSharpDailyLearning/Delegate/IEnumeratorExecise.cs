97a453396896936a3abeb0ad0b427b1340131a1a
baileicheng <xbfighting@hotmail.com>
2018-03-13 18:04:36 +0800

    [add] IEnumerable & IEnumerator

---
+++ CSharpDailyLearning/Delegate/IEnumeratorExecise.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class People : IEnumerable
+    {
+        private readonly Person[] _people;
+
+        public People(Person[] pArray)
+        {
+            _people = new Person[pArray.Length];
+
+            for (int i = 0; i < pArray.Length; i++)
+            {
+                _people[i] = pArray[i];
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            // 借用 PeopleEnumerator 实现 IEnumerable.GetEnumerator
+            //return new PeopleEnumerator(_people);
+
+            // yield 是一个语法糖，它的本质是为我们实现 IEnumerator 接口。
+            // yield 会自动实现 MoveNext,Reset,Current
+            foreach (var item in _people)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 实现 IEnumerator
+    /// </summary>
+    public class PeopleEnumerator : IEnumerator
+    {
+        public Person[] Peoples;
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                try
+                {
+                    return Peoples[_position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        private int _position = -1;
+
+        public PeopleEnumerator(Person[] peoples)
+        {
+            Peoples = peoples;
+        }
+
+        /// <summary>
+        /// MoveNext
+        /// </summary>
+        /// <returns></returns>
+        public bool MoveNext()
+        {
+            _position++;
+            return _position < Peoples.Length;
+        }
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public void Reset() => _position = -1;
+    }
+
+
+    /// <summary>
+    /// Person
+    /// </summary>
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ CSharpDailyLearning/DelegateTests/PeopleTests.cs
@@ -0,0 +1,55 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Delegate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Assert = NUnit.Framework.Assert;
+
+namespace Delegate.Tests
+{
+    [TestFixture]
+    public class PeopleTests
+    {
+        [Test]
+        public void PeopleTest()
+        {
+            // given
+            Person[] pArray =
+            {
+                new Person {FirstName = "F1", LastName = "L1"}, 
+                new Person {FirstName = "F2", LastName = "L2"}, 
+                new Person {FirstName = "F3", LastName = "L3"}, 
+            };
+            int excepted = 3;
+            int result = 0;
+
+            // when
+            var people = new People(pArray);
+            foreach (var item in people)
+            {
+                result++;
+            }
+
+            // then
+            Assert.AreEqual(excepted, result);
+        }
+
+        [Test]
+        public void GetEnumeratorTest()
+        {
+            Person[] pArray =
+           {
+                new Person {FirstName = "F1", LastName = "L1"},
+                new Person {FirstName = "F2", LastName = "L2"},
+                new Person {FirstName = "F3", LastName = "L3"},
+            };
+            
+            Assert.Fail();
+        }
+    }
+}

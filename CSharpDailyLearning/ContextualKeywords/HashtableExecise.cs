162c54b09cfbdcea2732331c13a01ea6492af660
baileicheng <xbfighting@hotmail.com>
2018-03-14 15:55:08 +0800

    [add] hashtable

---
+++ CSharpDailyLearning/ContextualKeywords/HashtableExecise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextualKeywords
+{
+    public class HashtableExecise
+    {
+        public int SimpleHash(string s, string[] arr)
+        {
+            int tot = 0;
+            char[] cname;
+            cname = s.ToCharArray();
+            var tempBound = cname.GetUpperBound(0);
+            for (int i = 0; i <= tempBound; i++)
+            {
+                tot += (Int32) cname[i];
+            }
+
+            var arrBound = arr.GetUpperBound(0);
+            var result = tot%arrBound;
+            return result;
+        }
+
+        public void ShowDistrib(string[] arr)
+        {
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+                if (arr[i] != null)
+                    Console.WriteLine(i + " " + arr[i]);
+        }
+    }
+}
---
+++ CSharpDailyLearning/ContextualKeywordsTests/HashtableExeciseTests.cs
@@ -0,0 +1,46 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContextualKeywords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ContextualKeywords.Tests
+{
+    [TestFixture]
+    public class HashtableExeciseTests
+    {
+        [Test]
+        public void SimpleHashTest()
+        {
+            // given
+            var hte = new HashtableExecise();
+
+            string[] names = new string[99];
+            string name;
+            string[] someNames =
+            {
+                "David", "Jennifer", "Donnie", "Mayo", "Raymond", "Bernica", "Mike", "Clayton",
+                "Beata", "Michael"
+            };
+            int hashVal;
+
+            // when
+            for (int i = 0; i < 10; i++)
+            {
+                name = someNames[i];
+                hashVal = hte.SimpleHash(name, names);
+                names[hashVal] = name;
+            }
+            hte.ShowDistrib(names);
+        }
+
+        [Test]
+        public void ShowDistribTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

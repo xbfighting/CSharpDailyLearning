883eb99d25ae8d0da5d6bf6d5c9b4c33cd16a90d
baileicheng <xbfighting@hotmail.com>
2018-03-21 17:53:33 +0800

    [add]  cancellation temp

---
+++ CSharpDailyLearning/ThreadBaseTests/CancelTaskTest.cs
@@ -0,0 +1,27 @@
+﻿using ThreadBase;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace ThreadBaseTests
+{
+    [TestFixture]
+    public class CancelTaskTest
+    {
+        /// <summary>
+        /// 消除
+        /// </summary>
+        [Test]
+        public void CancellationToken()
+        {
+            
+        }
+    }
+}

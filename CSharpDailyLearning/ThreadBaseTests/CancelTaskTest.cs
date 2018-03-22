86a2bafc670a490a97b1b332a6a33675f67ead55
baileicheng <xbfighting@hotmail.com>
2018-03-22 10:37:32 +0800

    [mod] Task.Factory.StartNew() 简写版 Task.Run()

--- CSharpDailyLearning/ThreadBaseTests/CancelTaskTest.cs
+++ CSharpDailyLearning/ThreadBaseTests/CancelTaskTest.cs
@@ -16,6 +16,34 @@ namespace ThreadBaseTests
     public class CancelTaskTest
     {
         /// <summary>
+        /// Task.Factory.StartNew() 简写版 Task.Run()
+        /// </summary>
+        [Test]
+        public void TaskFactoryNewRunTest()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Task.Factory.StartNew()");
+            });
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Task.Run()");
+            });
+
+            // 不推荐这种写法
+            var task = new Task(() =>
+            {
+                Thread.Sleep(1100);
+                Console.WriteLine("new task Start()");
+            });
+            task.Start();
+            task.Wait();
+        }
+
+        /// <summary>
         /// 消除
         /// </summary>
         [Test]
